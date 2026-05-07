0b3ca879dc132fa167c537094f36bebe6d0a149e
헤삐 <hyebbi@hyebbi.local>
2026-05-07 18:59:11 +0900

    add google login and firestore setup

--- app/firebase.ts
+++ app/firebase.ts
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCt8YU58aCrIxGwLG8DFw6KIkZkO_W86EA",
@@ -13,12 +14,12 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
+export const db = getFirestore(app);
+
+export const googleProvider = new GoogleAuthProvider();
+googleProvider.addScope("https://www.googleapis.com/auth/calendar.events");
+googleProvider.setCustomParameters({ prompt: "consent" });
 
 export const calendarProvider = new GoogleAuthProvider();
-
-calendarProvider.addScope(
-  "https://www.googleapis.com/auth/calendar.events"
-);
-calendarProvider.setCustomParameters({
-  prompt: "consent",
-});
+calendarProvider.addScope("https://www.googleapis.com/auth/calendar.events");
+calendarProvider.setCustomParameters({ prompt: "consent" });
